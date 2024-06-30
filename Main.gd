e1f19fff47777fec769591d7ebe6f036a8860765
jweimerjr46 <jweimerjr@gmail.com>
2024-06-30 15:35:31 -0500

    opens a json and appends question, need to make sure a file is selected before you click on save question

--- Main.gd
+++ Main.gd
@@ -15,6 +15,12 @@ extends Control
 @onready var get_image = $MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/FileDialog
 @onready var image_preview_label = $MarginContainer/HBoxContainer/VBoxContainer/ImagePreviewLabel
 var image_file_name = ""
+@onready var get_json = $MarginContainer2/MainMenu/FileDialogJSON
+var questions_json = ""
+
+var json
+var question_dict = {}
+
 
 func _ready():
 	pass
@@ -40,19 +46,18 @@ func _on_create_question_button_pressed():
 		elif option_4_checkbox.is_pressed():
 			correct_answer = option_4.text
 		else:
-			print("You have not selected anything")		
-		var question = { "question":question_text.text,
+			print("You have not selected anything")
+		question_dict = { "question":question_text.text,
 		 "options":[option_1.text, option_2.text, option_3.text, option_4.text],
 		"correct": correct_answer, "xp": xp_text.text, "level": level_text.text, "image": image_file_name}
-		var json_string = JSON.stringify(question, "\t")
+		var json_string = JSON.stringify(question_dict, "\t")
 		print(json_string)
-		var json = JSON.new()
-		json.parse(json_string)
-		print(json.data)
+		#json = JSON.new()
+		#json.parse(json_string_question)
+		#print(json.data)
 		preview_text.text = json_string
 	else:
 		print("You have not checked an answer")
-
 
 
 func _on_select_image_button_pressed():
@@ -63,3 +68,35 @@ func _on_file_dialog_file_selected(path):
 	image_file_name = get_image.current_file
 	image_preview_label.text = "[center][img]"+path+"[/img][/center]"	
 	print(get_image.current_file)
+
+
+func _on_file_dialog_json_file_selected(path):
+	questions_json = path
+	print(path)
+
+
+func _on_file_id_pressed(id):
+	print(id)
+	if id == 0:
+		get_json.visible = true
+		
+
+func add_question_to_json(question):
+	print(questions_json)
+	var file = FileAccess.open(questions_json, FileAccess.READ)
+	print(file)
+	var json_data = file.get_as_text()
+	file.close()
+	var parsed_data = JSON.new()
+	parsed_data.parse(json_data)
+	var questions_array = parsed_data.data
+	print(questions_array)
+	questions_array.append(question)
+	var json_string = JSON.stringify(questions_array, "\t")
+	file = FileAccess.open(questions_json, FileAccess.WRITE)
+	file.store_string(json_string)
+	file.close()
+
+
+func _on_save_question_pressed():
+	add_question_to_json(question_dict)
